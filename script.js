57cd4671c3f776fa30c565ad2b38da22b468044b
Oluwatobiloba <alomajatobi2001@gmail.com>
2025-05-12 09:03:49 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+adjective = "huge";
+noun = "rat";
+verb = "cold";
+place = "cave";
+adjective2 = "huge";
+noun2 = "carrots";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "."+ " The " + noun + " lived in a " + place + " and had "+ adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "sparkling";
+noun = "unicorn";
+verb = "dancing";
+place = " magical forest";
+adjective2 = "glowing";
+noun2 = "rainbow cupcakes";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "."+ " The " + noun + " lived in a " + place + " and had "+ adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
